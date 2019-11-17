af5b86deeb0ebe316d04e7d92ab3dcaff2ebe229
Krzysiek93 <35534607+Krzysiek93@users.noreply.github.com>
2019-11-17 11:35:57 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
 from serial import Serial
 import os
+import argparse
+import time
 
-ser = Serial("./ttydevice") #device
-readline = lambda : iter(lambda:ser.read(1),"\n")
-path = "/home/krzysztof/PycharmProjects/zadanie1/files/" #path to files
-name_dir_list = os.listdir(path)
 name_list = []
 
 
 class Server(object):
 
-    def __init__(self):
+    def __init__(self, device, path):
+        ser = Serial(device)  # device
+        self.readline = lambda: iter(lambda: ser.read(1), "\n")
+        name_dir_list = os.listdir(path)
+
         self.waiting_for_message("<<SENDFILE>>")
         for element in name_dir_list:
             stat_info = os.stat(path+'/'+element)
@@ -25,9 +27,17 @@ class Server(object):
             ser.write("\n<<EOF>>\n") # sending file end
 
     def waiting_for_message(self, message):
-        while "".join(readline()) != message:
+        timeout = time.time() + 60 * 5  # 5 minutes from now
+        while "".join(self.readline()) != message or time.time() > timeout:
             pass
 
 
 if __name__ == '__main__':
-    serv = Server()
+    parser =argparse.ArgumentParser()
+    parser.add_argument("--device", type=str, default="./ttydevice", help="Enter serial device")
+    parser.add_argument("--path", type=str, default="/home/krzysztof/PycharmProjects/zadanie1"
+                                                    "/files/", help="Enter path to artifacts")
+    args = parser.parse_args()
+    path = args.path
+    device = args.device
+    serv = Server(device, path)
